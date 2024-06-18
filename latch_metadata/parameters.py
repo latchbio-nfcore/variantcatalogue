fa39f930ea7b2e4793e9fe9a88e97d7098b0f36c
Tahir D'Mello <tahirdme@gmail.com>
2024-06-17 19:21:38 -0700

    Automated commit: processed repository variantcatalogue

---
+++ latch_metadata/parameters.py
@@ -0,0 +1,60 @@
+
+from dataclasses import dataclass
+import typing
+import typing_extensions
+
+from flytekit.core.annotation import FlyteAnnotation
+
+from latch.types.metadata import NextflowParameter
+from latch.types.file import LatchFile
+from latch.types.directory import LatchDir, LatchOutputDir
+
+# Import these into your `__init__.py` file:
+#
+# from .parameters import generated_parameters
+
+generated_parameters = {
+    'input': NextflowParameter(
+        type=LatchFile,
+        default=None,
+        section_title='Input/output options',
+        description='Path to comma-separated file containing information about the samples in the experiment.',
+    ),
+    'outdir': NextflowParameter(
+        type=typing_extensions.Annotated[LatchDir, FlyteAnnotation({'output': True})],
+        default=None,
+        section_title=None,
+        description='The output directory where the results will be saved. You have to use absolute paths to storage on Cloud infrastructure.',
+    ),
+    'email': NextflowParameter(
+        type=typing.Optional[str],
+        default=None,
+        section_title=None,
+        description='Email address for completion summary.',
+    ),
+    'multiqc_title': NextflowParameter(
+        type=typing.Optional[str],
+        default=None,
+        section_title=None,
+        description='MultiQC report title. Printed as page header, used for filename if not otherwise specified.',
+    ),
+    'genome': NextflowParameter(
+        type=typing.Optional[str],
+        default=None,
+        section_title='Reference genome options',
+        description='Name of iGenomes reference.',
+    ),
+    'fasta': NextflowParameter(
+        type=typing.Optional[LatchFile],
+        default=None,
+        section_title=None,
+        description='Path to FASTA genome file.',
+    ),
+    'multiqc_methods_description': NextflowParameter(
+        type=typing.Optional[str],
+        default=None,
+        section_title='Generic options',
+        description='Custom MultiQC yaml file containing HTML including a methods description.',
+    ),
+}
+
---
+++ wf/entrypoint.py
@@ -0,0 +1,142 @@
+from dataclasses import dataclass
+from enum import Enum
+import os
+import subprocess
+import requests
+import shutil
+from pathlib import Path
+import typing
+import typing_extensions
+
+from latch.resources.workflow import workflow
+from latch.resources.tasks import nextflow_runtime_task, custom_task
+from latch.types.file import LatchFile
+from latch.types.directory import LatchDir, LatchOutputDir
+from latch.ldata.path import LPath
+from latch_cli.nextflow.workflow import get_flag
+from latch_cli.nextflow.utils import _get_execution_name
+from latch_cli.utils import urljoins
+from latch.types import metadata
+from flytekit.core.annotation import FlyteAnnotation
+
+from latch_cli.services.register.utils import import_module_by_path
+
+meta = Path("latch_metadata") / "__init__.py"
+import_module_by_path(meta)
+import latch_metadata
+
+@custom_task(cpu=0.25, memory=0.5, storage_gib=1)
+def initialize() -> str:
+    token = os.environ.get("FLYTE_INTERNAL_EXECUTION_ID")
+    if token is None:
+        raise RuntimeError("failed to get execution token")
+
+    headers = {"Authorization": f"Latch-Execution-Token {token}"}
+
+    print("Provisioning shared storage volume... ", end="")
+    resp = requests.post(
+        "http://nf-dispatcher-service.flyte.svc.cluster.local/provision-storage",
+        headers=headers,
+        json={
+            "storage_gib": 100,
+        }
+    )
+    resp.raise_for_status()
+    print("Done.")
+
+    return resp.json()["name"]
+
+
+
+
+
+
+@nextflow_runtime_task(cpu=4, memory=8, storage_gib=100)
+def nextflow_runtime(pvc_name: str, input: LatchFile, outdir: typing_extensions.Annotated[LatchDir, FlyteAnnotation({'output': True})], email: typing.Optional[str], multiqc_title: typing.Optional[str], genome: typing.Optional[str], fasta: typing.Optional[LatchFile], multiqc_methods_description: typing.Optional[str]) -> None:
+    try:
+        shared_dir = Path("/nf-workdir")
+
+
+
+        ignore_list = [
+            "latch",
+            ".latch",
+            "nextflow",
+            ".nextflow",
+            "work",
+            "results",
+            "miniconda",
+            "anaconda3",
+            "mambaforge",
+        ]
+
+        shutil.copytree(
+            Path("/root"),
+            shared_dir,
+            ignore=lambda src, names: ignore_list,
+            ignore_dangling_symlinks=True,
+            dirs_exist_ok=True,
+        )
+
+        cmd = [
+            "/root/nextflow",
+            "run",
+            str(shared_dir / "main.nf"),
+            "-work-dir",
+            str(shared_dir),
+            "-profile",
+            "docker",
+            "-c",
+            "latch.config",
+                *get_flag('input', input),
+                *get_flag('outdir', outdir),
+                *get_flag('email', email),
+                *get_flag('multiqc_title', multiqc_title),
+                *get_flag('genome', genome),
+                *get_flag('fasta', fasta),
+                *get_flag('multiqc_methods_description', multiqc_methods_description)
+        ]
+
+        print("Launching Nextflow Runtime")
+        print(' '.join(cmd))
+        print(flush=True)
+
+        env = {
+            **os.environ,
+            "NXF_HOME": "/root/.nextflow",
+            "NXF_OPTS": "-Xms2048M -Xmx8G -XX:ActiveProcessorCount=4",
+            "K8S_STORAGE_CLAIM_NAME": pvc_name,
+            "NXF_DISABLE_CHECK_LATEST": "true",
+        }
+        subprocess.run(
+            cmd,
+            env=env,
+            check=True,
+            cwd=str(shared_dir),
+        )
+    finally:
+        print()
+
+        nextflow_log = shared_dir / ".nextflow.log"
+        if nextflow_log.exists():
+            name = _get_execution_name()
+            if name is None:
+                print("Skipping logs upload, failed to get execution name")
+            else:
+                remote = LPath(urljoins("latch:///your_log_dir/nf_nf_core_variantcatalogue", name, "nextflow.log"))
+                print(f"Uploading .nextflow.log to {remote.path}")
+                remote.upload_from(nextflow_log)
+
+
+
+@workflow(metadata._nextflow_metadata)
+def nf_nf_core_variantcatalogue(input: LatchFile, outdir: typing_extensions.Annotated[LatchDir, FlyteAnnotation({'output': True})], email: typing.Optional[str], multiqc_title: typing.Optional[str], genome: typing.Optional[str], fasta: typing.Optional[LatchFile], multiqc_methods_description: typing.Optional[str]) -> None:
+    """
+    nf-core/variantcatalogue
+
+    Sample Description
+    """
+
+    pvc_name: str = initialize()
+    nextflow_runtime(pvc_name=pvc_name, input=input, outdir=outdir, email=email, multiqc_title=multiqc_title, genome=genome, fasta=fasta, multiqc_methods_description=multiqc_methods_description)
+
